c27c4d11dd870565024a252b018aa503421eb3f3
Jona Juarez <solamentejona@gmail.com>
2025-03-05 12:09:40 -0300

    colocando los productos

---
+++ JavaScript/main.js
@@ -0,0 +1,32 @@
+const placas = [
+    {
+        id: 1
+        nombre: "RTX3060"
+        precio: 333333
+    }
+    {
+        id: 2
+        nombre: "RX6800XT"
+        precio: 343434
+    }
+    {
+        id: 3
+        nombre: "RTX3050"
+        precio: 222222
+    }
+    {
+        id: 4
+        nombre: "RTX4060Ti"
+        precio: 400400
+    }
+    {
+        id: 5
+        nombre: "RTX4070Ti"
+        precio: 600000
+    }
+    {
+        id: 6
+        nombre: "RTX5080"
+        precio: 2000550
+    }
+]
